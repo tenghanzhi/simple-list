06f6e5c9980ae56a736d3368ad6e3a57d567ebd7
Hanzhi Teng <tenghanzhi@outlook.com>
2024-09-29 17:03:46 -0700

    Updates

---
+++ precache-manifest.001747d842290d50391f74e55e6d5c53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "118f790bc6341245a33d539d520877e3",
+    "url": "/simple-list/index.html"
+  },
+  {
+    "revision": "bc6ea67696ebb67804fb",
+    "url": "/simple-list/static/css/main.8166b74c.chunk.css"
+  },
+  {
+    "revision": "9478aa57a961fb1612e1",
+    "url": "/simple-list/static/js/2.93a55011.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/simple-list/static/js/2.93a55011.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc6ea67696ebb67804fb",
+    "url": "/simple-list/static/js/main.23fa4b67.chunk.js"
+  },
+  {
+    "revision": "666dac0e6a3cc33dc8ef",
+    "url": "/simple-list/static/js/runtime-main.acf82098.js"
+  }
+]);
